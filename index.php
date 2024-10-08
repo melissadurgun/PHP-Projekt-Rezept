bec9d497250779ef05948e68c0e2b6dd2c7c724a
melissadurgun <durgun.melissa@yahoo.de>
2024-10-09 00:32:03 +0100

    Added Github Guide

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-// https://education.github.com/git-cheat-sheet-education.pdf
+<!-- 
+
+Github Cheat Sheet: https://education.github.com/git-cheat-sheet-education.pdf
+
+------------------------------------------------------------------
+------------------------------------------------------------------
+
+Pushing your changes into the Repository: (In Terminal [Ctrl+ü])
+
+---1. Stage your changes:
+        git add .
+    (This stages all changes in the current directory)
+
+---2. Check status:
+        git status
+    (Displays the current state of your working directory and staging area)
+
+---3. Commit your changes:
+        git commit -m "your commit message"
+    (Commits the staged changes with a message describing the update)
+
+---4. Push to the repository:
+        git push
+            or
+        git push origin main
+    (Pushes your changes to the remote repository on the specified branch)
+
+---5. Check that push was successful:
+        git status
+    (Ensures your working directory is clean and no pending changes exist)
+
+-->
