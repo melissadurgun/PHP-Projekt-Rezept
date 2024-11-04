2d2ff02fad57fc5425235c3d7e568316b3bc3938
melissadurgun <durgun.melissa@yahoo.de>
2024-11-04 11:04:42 +0100

    Fixed Verbindung zur Detailansicht

--- pages/Benutzerverwaltung/functionals.php
+++ pages/Benutzerverwaltung/functionals.php
@@ -3,18 +3,18 @@
 
 //Klasse für die Datenbankverbindungen 
 class Db extends PDO
-  {
-     public function query(string $statement, $mode = PDO::FETCH_ASSOC, mixed ...$fetch_mode_args): PDOStatement|false
-     {
-         try {
-             return parent::query($statement, $mode, ...$fetch_mode_args);
-         } catch (PDOException $e) {
-             echo '<div style="color: red">DB ERROR: ' . $e->getMessage();
-             echo '<br/>Query: ' . $statement . '</div>';
-             return false;
-         }
-     }
-  }
+{
+    public function query(string $statement, $mode = PDO::FETCH_ASSOC, mixed ...$fetch_mode_args): PDOStatement|false
+    {
+        try {
+            return parent::query($statement, $mode, ...$fetch_mode_args);
+        } catch (PDOException $e) {
+            echo '<div style="color: red">DB ERROR: ' . $e->getMessage();
+            echo '<br/>Query: ' . $statement . '</div>';
+            return false;
+        }
+    }
+}
 
 
 ?>
